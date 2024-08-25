a0c4a27a859534eccafc38520956d3ccd0b3266f
Ant Weiss <anton@otomato.io>
2024-08-25 22:39:32 +0300

    Smaller test

--- k6/api-test.js
+++ k6/api-test.js
@@ -13,15 +13,9 @@ export const options = {
     ramping_up: {
       executor: "ramping-vus",
       stages: [
-        { duration: "10s", target: 20 },
-        { duration: "50s", target: 20 },
-        { duration: "50s", target: 40 },
-        { duration: "50s", target: 60 },
-        { duration: "50s", target: 80 },
-        { duration: "50s", target: 100 },
-        { duration: "50s", target: 120 },
-        { duration: "50s", target: 140 },
-        { duration: "5s", target: 0 }, // scale down. (optional)
+        { duration: "5s", target: 20 },
+        { duration: "5s", target: 60 },
+        { duration: "10s", target: 100 },
       ],
     },
   }
